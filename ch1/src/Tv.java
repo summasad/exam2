2fd06d8f360e149a88c4da189335233c6c2971b5
김현진 <summersad10@gmail.com>
2024-09-13 12:16:40 +0900

    getter 메소드 작성

--- ch1/src/Tv.java
+++ ch1/src/Tv.java
@@ -2,4 +2,17 @@ public class Tv {
     private String color;
     private int channel;
     private boolean power;
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getChannel() {
+        return channel;
+    }
+
+    public boolean isPower() {
+        return power;
+    }
+
 }
