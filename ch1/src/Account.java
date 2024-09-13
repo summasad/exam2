92f59c57d5f728a891e1e0a59729b560b42497f2
김현진 <summersad10@gmail.com>
2024-09-13 11:50:50 +0900

    Revert "getter / setter 추가"
    This reverts commit f149c3b2e96bf572ae367af5675dc458cdde9b2a.

--- ch1/src/Account.java
+++ ch1/src/Account.java
@@ -2,29 +2,4 @@ public class Account {
     private String owner;
     private String accountNo;
     private int balance;
-
-    public String getOwner() {
-        return owner;
-    }
-
-    public void setOwner(String owner) {
-        this.owner = owner;
-    }
-
-    public String getAccountNo() {
-        return accountNo;
-    }
-
-    public void setAccountNo(String accountNo) {
-        this.accountNo = accountNo;
-    }
-
-    public int getBalance() {
-        return balance;
-    }
-
-    public void setBalance(int balance) {
-        this.balance = balance;
-    }
-
 }
